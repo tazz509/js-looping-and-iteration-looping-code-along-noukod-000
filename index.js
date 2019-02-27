1272b5f191125ab535e687d694fa56a55a3459fa
carly <carly@noukod.com>
2019-02-27 19:46:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function printBadges(employees){
+    for(let i =0; i < employees.length; i++){
+        console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+    }
+    return employees;
+}
+
+function tailsNeverFails(){
+
+    let nTails = 0;
+    while(Math.random() >= 0.5){
+        nTails++;
+    }
+    return `You got ${nTails} tails in a row!`;
+}
